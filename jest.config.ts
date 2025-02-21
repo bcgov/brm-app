17688e91a4575fac6ac3382ed31b6cc994688b45
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2025-02-20 16:09:38 -0800

    Add custom jest config and setup.

--- jest.config.ts
+++ jest.config.ts
@@ -2,25 +2,38 @@ import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(js|jsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  moduleNameMapper: {
+    // "^antd(.*)$": "<rootDir>/node_modules/antd$1",
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^antd/es/(.*)$": "<rootDir>/node_modules/antd/lib/$1",
+    "^antd(.*)$": "<rootDir>/node_modules/antd/lib$1",
+  },
+  // transformIgnorePatterns: ["/node_modules/(?!(@ant-design|antd|rc-.*|@babel/runtime|d3.*)/.*)/"],
+  transformIgnorePatterns: ["/node_modules/(?!antd|@ant-design|rc-*|@babel/runtime).+\\.js$"],
+
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: true,
   collectCoverageFrom: [
-    "app/**/*.{ts,tsx}", // Include all TS/TSX files in src
-    "!app/**/*.d.ts", // Exclude type declaration files
-    "!app/**/*.{test,spec}.{ts,tsx}", // Exclude test files
-    "!app/**/index.{ts,tsx}", // Optionally exclude index files
-    "!**/node_modules/**", // Exclude dependencies
+    "app/**/*.{js,jsx,ts,tsx}",
+    "!app/**/*.d.ts",
+    "!app/**/*.{test,spec}.{js,jsx,ts,tsx}",
+    "!app/**/index.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
   ],
   coverageProvider: "v8",
-  testEnvironment: "jsdom",
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
